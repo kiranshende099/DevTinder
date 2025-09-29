50b59fb5e00bd18ac20af899faa90050fdef1064
kiranshende <shendekiran096@gmail.com>
2025-09-29 17:46:55 +0530

    exploring HTTP methods

--- src/app.js
+++ src/app.js
@@ -2,14 +2,30 @@ const express = require("express");
 
 const app = express();
 
-// app.use((req, res) => {
-//   res.send("Server Started 134");
-// });
-app.use("/hello", (req, res) => {
-  res.send("Hello from server");
-});
-app.use("/test", (req, res) => {
-  res.send("testing");
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Kiran", lastName: "Shende" });
 });
 
-app.listen(7777, () => console.log(`Server run on port 7777`));
+app.post("/user", (req, res) => {
+  res.send("Data added successfully");
+  console.log("Data added successfully");
+});
+
+app.put("/user", (req, res) => {
+  res.send("Data updated successfully");
+  console.log("Data added successfully");
+});
+
+app.patch("/user", (req, res) => {
+  res.send("Data partially updated successfully");
+  console.log("Data added successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data removed successfully");
+  console.log("Data removed successfully");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Server run on port ${PORT}`));
